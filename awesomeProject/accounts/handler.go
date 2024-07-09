dbd70ac700fd32c02da45743b47f2da3bde17ab9
ArtemovMichael <92886587+ArtemovMichael@users.noreply.github.com>
2024-07-09 21:32:35 +0300

    Необходимо написать http сервер по работе с аккаунтами и балансами. Для этого можно использовать, как стандартную библиотеку net/http, так и фрейворки echo, gin-gonic, fiber, fasthttp и т.д. Должны быть реализованы 5 методов (получение аккаунта, изменение имени аккаунта, изменение баланса аккаунта, создание и удаление аккаунта). Далее должен быть реализован CLI (command-line interface), он же клиент. В нем для реализации интерфейса использовать можно стандартную библиотеку flag или любую внешнюю, например cobra. Если будете дорабатывать примеры, нужно статические функции пернести в методы структуры Command.

---
+++ awesomeProject/accounts/handler.go
@@ -0,0 +1,138 @@
+package accounts
+
+import (
+	"awesomeProject/accounts/dto"
+	"awesomeProject/accounts/models"
+	"net/http"
+	"sync"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func New() *Handler {
+	return &Handler{
+		accounts: make(map[string]*models.Account),
+		guard:    &sync.RWMutex{},
+	}
+}
+
+type Handler struct {
+	accounts map[string]*models.Account
+	guard    *sync.RWMutex
+}
+
+func (h *Handler) CreateAccount(c *fiber.Ctx) error {
+	var request dto.CreateAccountRequest
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse request"})
+	}
+
+	if len(request.Name) == 0 {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Name is empty"})
+	}
+
+	h.guard.Lock()
+
+	if _, ok := h.accounts[request.Name]; ok {
+		h.guard.Unlock()
+
+		return c.Status(http.StatusConflict).JSON(fiber.Map{"error": "Account already exists"})
+	}
+
+	h.accounts[request.Name] = &models.Account{
+		Name:   request.Name,
+		Amount: request.Amount,
+	}
+
+	h.guard.Unlock()
+
+	return c.Status(http.StatusCreated).JSON(fiber.Map{"message": "Account created"})
+}
+
+func (h *Handler) GetAccount(c *fiber.Ctx) error {
+	name := c.Params("name")
+
+	h.guard.RLock()
+
+	account, ok := h.accounts[name]
+
+	h.guard.RUnlock()
+
+	if !ok {
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Account not found"})
+	}
+
+	response := dto.GetAccountResponse{
+		Name:   account.Name,
+		Amount: account.Amount,
+	}
+
+	return c.JSON(response)
+}
+
+func (h *Handler) DeleteAccount(c *fiber.Ctx) error {
+	name := c.Params("name")
+
+	h.guard.Lock()
+
+	if _, ok := h.accounts[name]; !ok {
+		h.guard.Unlock()
+
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Account not found"})
+	}
+
+	delete(h.accounts, name)
+
+	h.guard.Unlock()
+
+	return c.JSON(fiber.Map{"message": "Account deleted"})
+
+}
+
+func (h *Handler) UpdateAccountName(c *fiber.Ctx) error {
+	old_name := c.Params("name")
+	new_name := new(dto.ChangeAccountRequest)
+
+	if err := c.BodyParser(&new_name); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse request"})
+	}
+
+	h.guard.Lock()
+
+	if _, ok := h.accounts[old_name]; !ok {
+		h.guard.Unlock()
+
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Account not found"})
+	}
+
+	h.accounts[new_name.NewName] = h.accounts[old_name]
+	h.accounts[new_name.NewName].Name = new_name.NewName
+	delete(h.accounts, old_name)
+
+	h.guard.Unlock()
+
+	return c.JSON(fiber.Map{"message": "Account name updated"})
+}
+
+func (h *Handler) UpdateAccountAmount(c *fiber.Ctx) error {
+	name := c.Params("name")
+	new_amount := new(dto.ChangeAccountAmountRequest)
+
+	if err := c.BodyParser(&new_amount); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse request"})
+	}
+
+	h.guard.Lock()
+
+	if _, ok := h.accounts[name]; !ok {
+		h.guard.Unlock()
+
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Account not found"})
+	}
+
+	h.accounts[name].Amount = new_amount.Amount
+
+	h.guard.Unlock()
+
+	return c.JSON(fiber.Map{"message": "Account amount updated"})
+}
